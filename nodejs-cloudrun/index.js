54bbf1d2da7dd857ddea7bb56ea4fcbd504e63bf
xke <xke@groveflow.com>
2022-11-23 22:58:45 -0800

    "02_04 Container images (Docker)" start state

---
+++ nodejs-cloudrun/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World with Cloud Run');
+});
+
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
