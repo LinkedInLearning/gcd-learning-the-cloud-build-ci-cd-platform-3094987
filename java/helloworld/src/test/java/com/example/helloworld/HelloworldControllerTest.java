e96f3afc497c8925137f67e405726c39595f0ca5
xke <xke@groveflow.com>
2022-11-24 14:26:58 -0800

    "03_04 Testing Java apps" end state

---
+++ java/helloworld/src/test/java/com/example/helloworld/HelloworldControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.helloworld;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloworldControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.ALL))
+				.andExpect(content().string(equalTo("Hello World")));
+	}
+}
