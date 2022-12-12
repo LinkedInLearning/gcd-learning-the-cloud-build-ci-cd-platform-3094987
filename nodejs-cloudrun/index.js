c36686fe4a68ca65868c89f48bd4cceb731e46d9
xke <xke@groveflow.com>
2022-12-12 15:32:13 -0800

    "02_05 Cloud Build and Artifacts" begin state

--- nodejs-cloudrun/index.js
+++ nodejs-cloudrun/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World with Cloud Build and Cloud Run and Artifact Registry');
+  res.send('Hello World with Cloud Build and Cloud Run');
 });
 
 const port = parseInt(process.env.PORT) || 8080;
